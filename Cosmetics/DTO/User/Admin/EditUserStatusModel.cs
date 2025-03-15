a1c8122884da48ae5b8b70891e34be8511cc2b58
Duychamchi <duynhon2106@gmail.com>
2025-03-15 15:44:27 +0700

    Admin
    Delete some thing dont need

--- Cosmetics/DTO/User/Admin/EditUserStatusModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Cosmetics.DTO.User.Admin
-{
-    public class EditUserStatusModel
-    {
-        public string Email { get; set; } 
-        public int UserStatus { get; set; } 
-    }
-}
