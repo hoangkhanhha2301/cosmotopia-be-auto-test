565351d1b631461f39a812859eb9e43bf92bb019
Duychamchi <duynhon2106@gmail.com>
2025-03-15 19:17:57 +0700

    ResetPassWord
    User

---
+++ Cosmetics/DTO/User/ResetPasswordModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Cosmetics.DTO.User
+{
+    public class ResetPasswordModel
+    {
+
+        public string Email { get; set; }
+    }
+
+    public class SetNewPasswordModel
+    {
+        public string Token { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
