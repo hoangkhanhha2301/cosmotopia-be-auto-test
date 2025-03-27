ef7da79b162d03d48bef2b789888fab27a782479
EinsteinDavang <hungnpse170107@fpt.edu.vn>
2025-03-28 02:51:23 +0700

    RequestPayment

--- Cosmetics/DTO/Payment/PaymentRequestDTO.cs
+++ Cosmetics/DTO/Payment/PaymentRequestDTO.cs
@@ -3,8 +3,5 @@
     public class PaymentRequestDTO
     {
         public Guid OrderId { get; set; }
-        public decimal Amount { get; set; }
-        public string ReturnUrl { get; set; }
-        public string PaymentMethod { get; set; }
     }
 }
