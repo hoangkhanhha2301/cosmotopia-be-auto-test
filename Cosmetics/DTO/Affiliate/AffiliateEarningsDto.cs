fafb4a83ce76d10863f18592f9953163ef774daf
Duychamchi <duynhon2106@gmail.com>
2025-03-28 19:37:41 +0700

    GetTotaol

--- Cosmetics/DTO/Affiliate/AffiliateEarningsDto.cs
+++ Cosmetics/DTO/Affiliate/AffiliateEarningsDto.cs
@@ -5,6 +5,9 @@
         public Guid ProductId { get; set; }
         public string ProductImageUrl { get; set; }
         public string ReferralCode { get; set; }
+
+        public int TotalClicks { get; set; }
         public decimal TotalEarnings { get; set; }
+
     }
 }
--- Cosmetics/DTO/Affiliate/AffiliateStatsDto.cs
+++ Cosmetics/DTO/Affiliate/AffiliateStatsDto.cs
@@ -7,4 +7,10 @@
         public int ConversionCount { get; set; }
         public double ConversionRate => Count > 0 ? (double)ConversionCount / Count : 0;
     }
+
+    public class AffiliateSummaryDto
+    {
+        public decimal TotalEarnings { get; set; }
+        public int TotalClicks { get; set; }
+    }
 }
